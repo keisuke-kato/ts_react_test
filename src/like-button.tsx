81684ec13c0c26196b24b85735969f7e865b1f09
keisuke-kato <k.kato@2-bee.com>
2026-05-29 14:05:18 +0900

    初回コミット

---
+++ src/like-button.tsx
@@ -0,0 +1,18 @@
+
+
+import "./like-button.css";
+import { useState } from "react";
+
+export function LikeButton() {
+  const [count, setCount] = useState(0);
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+  return (
+    <button className="like" onClick={handleClick} type="button">
+      {count}
+    </button>
+  );
+}
+
+
